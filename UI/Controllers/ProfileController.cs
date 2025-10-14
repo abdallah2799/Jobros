24eda098b895921a5851b9dc2e6b757216b92bce
Arwa <arwayahya@gmail.com>
2025-10-14 23:39:42 +0300

    UI: Splash Page, Login, Register, About, Contact

---
+++ UI/Controllers/ProfileController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UI.Models;
+
+
+namespace UI.Controllers
+{
+    public class ProfileController : Controller
+    {
+
+        public IActionResult Profile()
+        {
+            var role = User.IsInRole("Employer") ? "Employer" : "JobSeeker";
+            ViewBag.Role = role;
+            return View();
+        }
+
+    }
+}
