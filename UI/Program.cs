fe181b9be8d533c16513fe76f033dabeaaec0750
Abdullah Ragab <abdullah.ragab2799@gmail.com>
2025-10-15 08:56:33 +0300

    Feat : Add DB Initializer

--- UI/Program.cs
+++ UI/Program.cs
@@ -1,19 +1,47 @@
+﻿using Core.Entities;
 using Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Connection String
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+// Register DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
-// Add services to the container.
+// Register Identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole<int>>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 6;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+// Add MVC Controllers + Views
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Automatic Seeding on Startup
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+    var roleManager = services.GetRequiredService<RoleManager<IdentityRole<int>>>();
+    var context = services.GetRequiredService<ApplicationDbContext>();
+
+    await DbInitializer.SeedAsync(userManager, roleManager, context);
+}
+
+// Configure Middleware
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -21,18 +49,22 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles(); // <-- Add this line
+
+// Serve static files (wwwroot)
+app.UseStaticFiles();
+
 app.UseRouting();
 
+// Identity Middleware
+app.UseAuthentication();
 app.UseAuthorization();
 
-// This .WithStaticAssets() works with MapStaticAssets(), but UseStaticFiles() is standard for wwwroot.
-// It's good to have both if your template uses them.
-app.MapStaticAssets();
-
+// Map Routes
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// If your template uses it (optional, for static assets mapping)
+app.MapStaticAssets();
 
 app.Run();
