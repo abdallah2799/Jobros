795bded3eb8e6948c42375badfd5ec7dc96b50c1
Abdullah Ragab <abdullah.ragab2799@gmail.com>
2025-10-16 07:05:33 +0300

    Feat : Set Main Interfaces For Phase 2

---
+++ Core/Interfaces/IRepositories/ISpecification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Core.Interfaces.IRepositories
+{
+    /// <summary>
+    /// A small Specification contract for reusable filter expressions.
+    /// Repositories may offer FindAsync(ISpecification{T}) overloads that use this.
+    /// </summary>
+    public interface ISpecification<T>
+    {
+        Expression<Func<T, bool>> Criteria { get; }
+    }
+}
