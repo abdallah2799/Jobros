642432bf16f62152f9179e0c648c752097c60dae
sohilasalah16 <120594461+sohilasalah16@users.noreply.github.com>
2025-10-16 23:12:48 +0300

    create repository class and unitofwork

---
+++ Infrastructure/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using Core.Entities;
+using Core.Interfaces.IRepositories;
+using Core.Interfaces.IUnitOfWorks;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.UnitOfWorks
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+       public ApplicationDbContext db;
+         public UnitOfWork(ApplicationDbContext db)
+          {
+                this.db = db;
+        }
+
+        IRepository<Job> IUnitOfWork.Jobs => throw new NotImplementedException();
+
+        IRepository<Category> IUnitOfWork.Categories => throw new NotImplementedException();
+
+        IRepository<Application> IUnitOfWork.Applications => throw new NotImplementedException();
+
+        Task<int> IUnitOfWork.CompleteAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDisposable.Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
