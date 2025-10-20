1f8350f39ae09ee6a9656e0619ac34718dad7bec
Mohammed.E Mekkawy <mohammedessam9922@gmail.com>
2025-10-20 10:44:54 +0300

    add IjobSeekerCommandService,IjobSeekerQueryService and update on IunitOfWork

--- Core/Interfaces/IServices/Commands/IJobSeekerCommandService.cs
+++ Core/Interfaces/IServices/Commands/IJobSeekerCommandService.cs
@@ -5,6 +5,9 @@ namespace Core.Interfaces.IServices.Commands
 {
     public interface IJobSeekerCommandService
     {
-       
+        Task<ApplicationDto> ApplyAsync(int jobSeekerId, ApplicationCreateDto dto);
+        Task<bool> DeleteApplicationAsync(int jobSeekerId, int applicationId);
+        Task<bool> UpdateProfileAsync(int jobSeekerId, Core.DTOs.JobSeeker.JobSeekerUpdateDto dto);
+        Task<bool> ChangePasswordAsync(int jobSeekerId, string currentPassword, string newPassword);
     }
 }
