650df5cbf1915b1c1480181c1ae8057918c0f60c
Mohammed.E Mekkawy <mohammedessam9922@gmail.com>
2025-10-20 10:23:06 +0300

    error fix

--- Core/Interfaces/IServices/Commands/IJobSeekerCommandService.cs
+++ Core/Interfaces/IServices/Commands/IJobSeekerCommandService.cs
@@ -1,5 +1,4 @@
 using Core.DTOs.Application;
-using Core.DTOs.JobSeeker;
 using System.Threading.Tasks;
 
 namespace Core.Interfaces.IServices.Commands
--- Core/Interfaces/IServices/IQueries/IJobSeekerQueryService.cs
+++ Core/Interfaces/IServices/IQueries/IJobSeekerQueryService.cs
@@ -1,5 +1,4 @@
 using Core.DTOs.Application;
-using Core.DTOs.JobSeeker;
 using Core.DTOs.Job;
 using System.Collections.Generic;
 using System.Threading.Tasks;
