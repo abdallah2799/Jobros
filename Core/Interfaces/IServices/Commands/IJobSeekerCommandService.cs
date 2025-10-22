1ac85357927ae203852cb092a3385137b0211945
Mohammed.E Mekkawy <mohammedessam9922@gmail.com>
2025-10-22 17:59:23 +0300

    Revert "update : IJobSeekerQueryService"
    This reverts commit 61b77fde208f4bdc18247855367c39b9c08a08eb.

--- Core/Interfaces/IServices/Commands/IJobSeekerCommandService.cs
+++ Core/Interfaces/IServices/Commands/IJobSeekerCommandService.cs
@@ -1,33 +1,13 @@
 using Core.DTOs.Application;
-using Core.DTOs.Job;
 using System.Threading.Tasks;
 
 namespace Core.Interfaces.IServices.Commands
 {
     public interface IJobSeekerCommandService
     {
-        Task<IEnumerable<JobDto>> GetActiveJobsAsync(
-            string? keyword = null,
-            int? categoryId = null,
-            string? employerName = null,
-            string? location = null,
-            string? jobType = null,
-            int page = 1,
-            int pageSize = 10);
-
-        Task<int> GetActiveJobsTotalCountAsync(
-            string? keyword = null,
-            int? categoryId = null,
-            string? employerName = null,
-            string? location = null,
-            string? jobType = null);
-
-        Task<JobDto?> GetJobByIdAsync(int jobId);
-
-        Task<bool> HasAppliedAsync(int jobSeekerId, int jobId);
-
-        Task<IEnumerable<ApplicationDto>> GetApplicationsByJobSeekerAsync(int jobSeekerId);
-
-        Task<Core.DTOs.JobSeeker.JobSeekerDto?> GetProfileAsync(int jobSeekerId);
+        Task<ApplicationDto> ApplyAsync(int jobSeekerId, ApplicationCreateDto dto);
+        Task<bool> DeleteApplicationAsync(int jobSeekerId, int applicationId);
+        Task<bool> UpdateProfileAsync(int jobSeekerId, Core.DTOs.JobSeeker.JobSeekerUpdateDto dto);
+        Task<bool> ChangePasswordAsync(int jobSeekerId, string currentPassword, string newPassword);
     }
 }
